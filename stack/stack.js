6c104d3bac9961c7910ca3c5885737f853f49677
Yury Karalkou <yury.karalkou@gmail.com>
2017-10-31 08:43:54 +0300

    Pull changes from mentor's repository. Replace old babel preset

--- stack/stack.js
+++ stack/stack.js
@@ -1,20 +1,28 @@
 import Node from './node';
 
-export default class Stack {
-  constructor() {
-    this.top = null;
-    this.length = 0;
-  }
+class Stack {
+    constructor() {
+        this.top = null;
+        this.length = 0;
+    }
 
-  pop() {
     // delete the top element of stack
-  }
+    pop() {
+        let current = this.top;
+        this.top = current.next;
+        this.length--;
+    }
 
-  push(value) {
     // add element on top of stack
-  }
+    push(value) {
+        let currentNode = this.top;
+        this.top = new Node(value);
+        this.top.next = currentNode;
+        this.length++;
+    }
 
-  peek() {
     // returns value of top stack element
-  }
+    peek() {
+        return this.top ? this.top.value : null;
+    }
 }

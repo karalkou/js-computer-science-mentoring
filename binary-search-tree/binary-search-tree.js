b86273dfb7b3a7ee5fff0b13fe290d91f0a0df58
Yury Karalkou <yury.karalkou@gmail.com>
2017-11-06 12:26:43 +0300

    [YK] master: Copy binary-search-tree from https://github.com/cheekeebreekee/js-computer-science-mentoring

---
+++ binary-search-tree/binary-search-tree.js
@@ -0,0 +1,37 @@
+import Node from './node';
+
+export default class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    root() {
+        // returns root element of bst
+    }
+
+    insert(value) {
+        // insert node to bst
+    }
+
+    delete(data) {
+        // delete node from bst
+    }
+
+
+    search(value) {
+        // return node with value of argument
+    }
+
+
+    contains(value) {
+        // returns true if tree contains node with value from argument
+    }
+
+    traverse() {
+
+    }
+
+    verify() {
+
+    }
+}
---
+++ binary-search-tree/node.js
@@ -0,0 +1,7 @@
+export default class Node {
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}

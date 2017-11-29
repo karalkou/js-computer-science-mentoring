7a2f9cb4edb31d3c82f4dbc81a6e4ffed4723e80
Karalkou <yury.karalkou@gmail.com>
2017-11-29 07:30:30 +0300

    Start to create binary search tree class

--- binary-search-tree/binary-search-tree.js
+++ binary-search-tree/binary-search-tree.js
@@ -7,31 +7,85 @@ export default class BinarySearchTree {
 
     root() {
         // returns root element of bst
+        if ( !this.root ) throw new Error('there is no root element');
+        return this.root;
     }
 
-    insert(value) {
+    insert(data) {
         // insert node to bst
+        let node = new Node(data);
+        if(!this.root) {
+            this.root = node;
+        } else {
+            let current = this.root;
+
+            while(current) {
+                if(node.data < current.data) {
+                    if(!current.left) {
+                        current.left = node;
+                        break;
+                    }
+                    current = current.left;
+                } else if (node.data > current.data) {
+                    if(!current.right) {
+                        current.right = node;
+                        break;
+                    }
+                    current = current.right;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
-    delete(data) {
+    removeAt(data) {
         // delete node from bst
     }
 
-
-    search(value) {
-        // return node with value of argument
+    search(data) {
+        // return node with data of argument
     }
 
-
-    contains(value) {
-        // returns true if tree contains node with value from argument
+    contains(data) {
+        // returns true if tree contains node with data from argument
+        let current = this.root;
+        while(current) {
+            if(data === current.data) {
+                return true;
+            }
+            if(data < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
     }
 
-    traverse() {
+    traverse() {}
 
+    verify() {}
+
+    getMax(node){
+        if(!node) {
+            node = this.root;
+        }
+        while(node.right) {
+            node = node.right;
+        }
+
+        return node.data;
     }
 
-    verify() {
+    getMin(node){
+        if(!node) {
+            node = this.root;
+        }
+        while(node.left) {
+            node = node.left;
+        }
 
+        return node.data;
     }
 }
